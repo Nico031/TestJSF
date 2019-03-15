bf36b1ad815b48ba10d1ca89415cb0fc461bcb1f
Nico031 <48243996+Nico031@users.noreply.github.com>
2019-03-15 09:27:31 +0100

    Internationalization

--- TestJSF/src/fr/afpa/HelloBean.java
+++ TestJSF/src/fr/afpa/HelloBean.java
@@ -4,20 +4,31 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 
-	@ManagedBean
-	@SessionScoped
+	@ManagedBean	// bean géré par JSF - respecter les convetions de nommage.
+	@SessionScoped	//stockée dans la session (Si @RequestScoped => stockée dans la requete )
+	
 	public class HelloBean implements Serializable {
 	 
 	    private static final long serialVersionUID = 1L;
 	 
 	    private String name;
+	    
+	    private String password;
 	 
-	    public String getName() {
+	    public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getName() {
 	        return name;
 	    }
 	    public void setName(String name) {
 	        this.name = name;
 	    }
+	    
+	   
 
 
 }
